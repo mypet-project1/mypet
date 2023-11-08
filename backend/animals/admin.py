db16d066a73a6452553e8b264f200bc41c40cc47
Lee816 <lgha0816@naver.com>
2023-11-09 01:56:50 +0900

    [feat] animals 앱 모델 admin 등록

--- backend/animals/admin.py
+++ backend/animals/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Animal
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["name"]
+    ordering = ["-created_at"]
