1fe762d3cdedd556e41edf3646d2f7c8d34bb880
Lee816 <lgha0816@naver.com>
2023-11-09 23:36:05 +0900

    [Feat] tips 앱 모델 추가 및 admin 등록 (#9)
    * [feat] tips 앱 Tip 모델 작성
    
    aacount 앱 User 모델 pass형태로 구현
    
    * [feat] tips 앱 Tip 모델 admin에 등록

--- backend/tips/admin.py
+++ backend/tips/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tip
+
+
+@admin.register(Tip)
+class TipAdmin(admin.ModelAdmin):
+    list_filter = ["user", "created_at"]
+    search_fields = ["animal", "user", "content"]
+    ordering = ["-updated_at"]
