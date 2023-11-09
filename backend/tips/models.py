55ad6aef9b9807bada0f5aafea12c875ce8e20f5
Lee816 <lgha0816@naver.com>
2023-11-09 23:33:58 +0900

    [feat] tips 앱 Tip 모델 작성
    aacount 앱 User 모델 pass형태로 구현

--- backend/tips/models.py
+++ backend/tips/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+from animals.models import Animal
+
+
+class Tip(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, related_name="tips")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tips")
+    content = models.TextField()
+    tip_media = models.FileField(blank=True, upload_to="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["animal"]),
+            models.Index(fields=["user"]),
+            models.Index(fields=["-created_at"]),
+            models.Index(fields=["-updated_at"]),
+        ]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"[{self.animal}] {self.user}의 팁"
