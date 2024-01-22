5bb82ed1b36dd108eeb8e2349a3fc335700504e4
Lee816 <lgha0816@naver.com>
2024-01-22 14:28:12 +0900

    [feat] tips 전체 목록 반환 뷰 작성

--- backend/tips/views.py
+++ backend/tips/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Tip
+from .serializers import TipSerializers
+
+
+class TipAllListAPIView(APIView):
+    def get(self, request):
+        tips = Tip.objects.all()
+        serializer = TipSerializers(tips, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
