62d5e11758ee8c89afd200bcad2eddff8af52344
Lee816 <lgha0816@naver.com>
2024-01-22 22:16:56 +0900

    [feat] tip의 미디어파일 경로와 수정,삭제시 파일도 삭제되도록 수정

--- backend/tips/models.py
+++ backend/tips/models.py
@@ -1,14 +1,20 @@
+import os
 from django.db import models
 
+from config import settings
 from accounts.models import User
 from animals.models import Animal
+
+
+def tip_media_path(instance, filename):
+    return f"tips/{instance.animal}/{instance.user}/{filename}"
 
 
 class Tip(models.Model):
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE, related_name="tips")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tips")
     content = models.TextField()
-    tip_media = models.FileField(blank=True, upload_to="")
+    tip_media = models.FileField(blank=True, upload_to=tip_media_path)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -23,3 +29,7 @@ class Tip(models.Model):
 
     def __str__(self):
         return f"[{self.animal}] {self.user}의 팁"
+
+    def delete(self, *args, **kwargs):
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.tip_media.path))
+        super(Tip, self).delete(*args, **kwargs)
