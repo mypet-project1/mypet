e271e25a54a8a262e7a1b53d976c29aae7abe308
Lee816 <lgha0816@naver.com>
2024-01-29 19:33:09 +0900

    [feat] 병원 및 리뷰 admin 사이트 등록

--- backend/hospital/admin.py
+++ backend/hospital/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hospital, Review
+
+
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ["name", "call", "address", "created_at"]
+    search_fields = ["name", "call", "address", "info", "created_at"]
+    ordering = ["name"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["hospital", "user", "title", "grade", "created_at"]
+    search_fields = ["hospital", "user", "title", "grade", "content"]
+    ordering = ["-created_at"]
