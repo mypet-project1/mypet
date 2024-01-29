51df3429e1b1ae3fbaa874cb7658c386c8f470fc
Lee816 <lgha0816@naver.com>
2024-01-29 19:05:47 +0900

    [feat] 병원 model 초안 작성

--- backend/hospital/models.py
+++ backend/hospital/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+from accounts.models import User
+
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    call = models.CharField(max_length=10)
+    info = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),
+            models.Index(fields=["call"]),
+            models.Index(fields=["-created_at"]),
+            models.Index(fields=["-updated_at"]),
+        ]
+        ordering = ["name"]
+
+    def str(self):
+        return f"{self.name} - {self.call}"
+
+
+class Review(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="hospital_review"
+    )
+    hospital = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE, related_name="review"
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["hospital"]),
+            models.Index(fields=["grade"]),
+            models.Index(fields=["-created_at"]),
+            models.Index(fields=["-updated_at"]),
+        ]
+        ordering = ["-created_at"]
+
+    def str(self):
+        return f"[{self.hospital}] {self.user} review"
