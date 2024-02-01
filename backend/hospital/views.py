8d665c2fd97fa4c9da4855c9dca43b78d3885fc8
Lee816 <lgha0816@naver.com>
2024-02-01 18:49:39 +0900

    [refactor] hospital 리스트, 디테일 반환 뷰 작성

--- backend/hospital/views.py
+++ backend/hospital/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from .serializers import HospitalSerializer, ReviewSerializer
+from .models import Hospital, Review
+
+
+class HospitalsAPIView(APIView):
+    def get(self, reqeust):
+        hospitals = Hospital.objects.all()
+        serializer = HospitalSerializer(hospitals, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class OnehospitalAPIView(APIView):
+    def get(self, request, id):
+        hospital = get_object_or_404(Hospital, id=id)
+
+        serializer = HospitalSerializer(hospital)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
