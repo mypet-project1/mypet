6cc02b8feeec7c46adfccdde44339dbf90d77a21
Lee816 <lgha0816@naver.com>
2024-02-01 18:49:13 +0900

    [refactor] hospital 시리얼라이저 수정

--- backend/hospital/serializers.py
+++ backend/hospital/serializers.py
@@ -3,13 +3,23 @@ from rest_framework import serializers
 from .models import Hospital, Review
 
 
-class HospitalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hospital
-        fields = "__all__"
-
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = "__all__"
+
+
+class HospitalSerializer(serializers.ModelSerializer):
+    review = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Hospital
+        fields = [
+            "name",
+            "address",
+            "road_address",
+            "call",
+            "info",
+            "updated_at",
+            "review",
+        ]
