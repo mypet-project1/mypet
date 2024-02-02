36b6b7b312738c5b75a24555d2c5625d7ef7cce2
Lee816 <lgha0816@naver.com>
2024-02-02 18:57:24 +0900

    [feat] chat앱 병원별 채팅방 반환 뷰 추가

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from accounts.models import User
+from hospital.models import Hospital
+from .models import HospitalChatRoom, Message
+from .serializers import HospitalChatRoomSerializers, MessageSerializers
+
+
+class HospitalChatRoomAPIView(APIView):
+    def get(self, request, id):
+        hospital = get_object_or_404(Hospital, id=id)
+        chatroom = get_object_or_404(
+            HospitalChatRoom, hospital=hospital, user=2
+        )  # user=request.user 로 교체
+
+        serializer = HospitalChatRoomSerializers(chatroom)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
