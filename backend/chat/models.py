53cc64fa2c9a338cb656c582d70f40e247e312f4
Lee816 <lgha0816@naver.com>
2024-02-02 17:35:59 +0900

    [feat] chat 앱 models 작성

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+from hospital.models import Hospital
+
+
+class HospitalChatRoom(models.Model):
+    hospital = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE, related_name="chat_room"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="hospital_chat_room"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["-created_at"]),
+            models.Index(fields=["hospital"]),
+            models.Index(fields=["user"]),
+        ]
+
+    def __str__(self):
+        return f"{self.hospital} - {self.user} ChatRoom"
+
+
+class Message(models.Model):
+    message = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="message")
+    chat_room = models.ForeignKey(
+        HospitalChatRoom, on_delete=models.CASCADE, related_name="message"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        indexes = [
+            models.Index(fields=["created_at"]),
+            models.Index(fields=["sender"]),
+            models.Index(fields=["chat_room"]),
+        ]
+
+    def __str__(self):
+        return f"{self.chat_room} - {self.sender} Message"
